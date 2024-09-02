7064ccfb96630fd3cb3a2c38808617b21a2ab6fb
Nagulan <nagulnagulan2003@gmail.com>
2024-09-02 15:54:31 +0100

    example code

--- Currency-Converter/src/App.jsx
+++ Currency-Converter/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import './App.css'
 import converter from './assets/converter.svg';
 import { useEffect } from 'react';
-
+//States
 function App() {
   const[amount,setAmount]=useState(1);
   const[fromCurrency,setFromCurrency]=useState("USD")
